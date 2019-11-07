f8225c20fb3ce51a3901a4e674e253a6a734da0f
ImL1s <aa22396584@gmail.com>
2019-11-07 18:52:11 +0800

    feat: test

--- contracts/TodoFactory.sol
+++ contracts/TodoFactory.sol
@@ -8,8 +8,15 @@ contract TodoFactory {
     }
 
     event OnTodoAdded(uint todoId);
+    event OnTodoCompleted(uint todoId);
+    event OnTodoNotCompleted(uint todoId);
 
     Todo[] todos;
+
+    modifier isValidTodo(uint _todoId){
+        require(_todoId != 0);
+        _;
+    }
 
     function addTodo(string memory _taskName) public {
         Todo memory todo = Todo(_taskName, false, true);
@@ -17,14 +24,17 @@ contract TodoFactory {
         emit OnTodoAdded(todoId);
     }
 
-    modifier isValidTodo(uint _todoId){
-        require(_todoId != 0);
-        _;
-    }
-
     function deleteTodo(uint _todoId) public isValidTodo(_todoId) {
         todos[_todoId].isValid = false;
     }
 
-
+    function finishTodo(uint _todoId) public isValidTodo(_todoId) {
+//        if (todos.length <= _todoId) {
+//            emit OnTodoNotCompleted(_todoId);
+//            return;add
+//        }
+//        Todo memory todo = todos[_todoId];
+//        todo.isCompleted = true;
+//        emit OnTodoCompleted(_todoId);
+    }
 }
--- test/test_todoFactory.js
+++ test/test_todoFactory.js
@@ -10,4 +10,24 @@ contract('TodoFactory', function(accounts) {
         // 驗證合約是否已經被佈署
         assert.isDefined(contract);
     });
+
+    // it('Should not complete invalid task', async () => {
+    //     contract.finishTodo(9527, (err) => {
+    //         assert.isDefined(err);
+    //     })
+    // });
+
+    it('Should add new todo properly', async () => {
+        // addTodo 的呼叫是 Transaction 所以即使 addTodo 中有回傳值，也無法收到
+        const tx1 = await contract.addTodo("Hello world!");
+        // const events1 = utils.getEvents(tx1, { event: 'OnTodoAdded', logIndex: 0 });
+        // todoId1 = events1[0].args.todoId;
+    });
 });
+
+
+function getEvents (tx, filter) {
+    const logs = tx.logs;
+    const events = _.filter(logs, filter);
+    return events;
+}
